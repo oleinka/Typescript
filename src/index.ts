4744695d6d761a55bf1be6941e6b80e42d34b4eb
Marta <m.olejniczak550@gmail.com>
2025-06-04 14:26:55 +0200

    enum with all values

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-//const small = 1;
-//const medium = 2;
-//const large = 3;
-
-//instead of that we can use enums
-
-enum Size { Small = 1, Medium, Large };
+enum Size { Small = 's', Medium = 'm', Large = 'l' };
