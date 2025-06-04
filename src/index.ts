361724a531a612beaec2be130c83ecf8271eb03f
Marta <m.olejniczak550@gmail.com>
2025-06-04 14:01:26 +0200

    arrays

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-let age: number;
-let time: any; 
+let numbers: number[] = [1, 2, 3]; 
+let numbers1: number[] = [];
+let names: string[] = ['Ada','Zosia','Madzia']; 
+let newArray = []; //any type
+newArray[0] = 1; 
+newArray[1] = 'Ada';
