5f72d52b489b73ea5bd1964a95b151893dd30a70
Marta <m.olejniczak550@gmail.com>
2025-06-04 14:36:10 +0200

    enum Size with new variable

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let user = [1, 'Ada'];
+var Size;
+(function (Size) {
+    Size["Small"] = "s";
+    Size["Medium"] = "m";
+    Size["Large"] = "l";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 enum Size { Small = 's', Medium = 'm', Large = 'l' };
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
