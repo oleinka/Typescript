f7a98ecd9dbda9b0dbb875611c60adab03c13347
Marta <m.olejniczak550@gmail.com>
2025-06-04 20:13:13 +0200

    function with default value of parameter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-function calculateTax (income: number): number {
-    let x;
-    if (income < 50_000)
+function calculateTax (income: number, taxYear = 2025): number {
+    if (taxYear < 2025)
         return income * 1.2;
     return income * 1.3;
 } 
+
+calculateTax (10_000);
