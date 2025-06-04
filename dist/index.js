04d577dae2d86713d7072e6659a63500508a2cb9
Marta <m.olejniczak550@gmail.com>
2025-06-04 14:39:01 +0200

    changing enum for const

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,5 @@
 "use strict";
-var Size;
-(function (Size) {
-    Size["Small"] = "s";
-    Size["Medium"] = "m";
-    Size["Large"] = "l";
-})(Size || (Size = {}));
 ;
-let mySize = Size.Medium;
+let mySize = "m";
 console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-enum Size { Small = 's', Medium = 'm', Large = 'l' };
+const enum Size { Small = 's', Medium = 'm', Large = 'l' };
 
 let mySize: Size = Size.Medium;
 console.log(mySize);
