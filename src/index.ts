7358276dcf415cbfec4f7305d50f2c72106c2a63
Marta <m.olejniczak550@gmail.com>
2025-06-04 14:21:34 +0200

    enum with initial first variable

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-let user: [number, string] = [1, 'Ada']; 
+//const small = 1;
+//const medium = 2;
+//const large = 3;
+
+//instead of that we can use enums
+
+enum Size { Small = 1, Medium, Large };
