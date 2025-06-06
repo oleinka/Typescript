d8c23c60b44d238500e9ba093a75ab6991c731e4
Marta <m.olejniczak550@gmail.com>
2025-06-06 13:36:59 +0200

    Intersection types

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,6 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === 'number')
-        return weight * 2.2;
-    else
-        return parseInt(weight) * 2.2;
-}
-kgToLbs(10);
-kgToLbs('10kg');
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
 //# sourceMappingURL=index.js.map
